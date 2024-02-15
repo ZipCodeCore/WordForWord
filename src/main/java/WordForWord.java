5c0882e197c5bd15566183f7daeb53f48b3295f3
Kristofer <kris@youngers.org>
2024-02-15 09:00:34 -0500

    map?

--- src/main/java/WordForWord.java
+++ src/main/java/WordForWord.java
@@ -1,7 +1,50 @@
 package src.main.java;
 
+/**
+ * Four phases of text processing to do. Write a method for each one.
+ *
+ * public void print()  - write a method that reads the contents of a file, line by line, and creates a String object,
+ * making sure all the newlines are preserved. use BufferedReader to do the reading.
+ *
+ * For the sake of this lab, words are not and do not have punctuation.
+ *
+ * public WCResult wc(String input)
+ * commonly called "wc"; count the number of characters in a file, number of words, number of lines.
+ * Returns an object of class WCResult (a POJO) which consists of the three longs you counted.
+ *
+ * public FrequencyMap wordFrequency(String input)
+ * word count. words in a file, produce a map with (String word, Long numOfTimes). FrequencyMap returning a HashMap might be what you're
+ * looking for here, or maybe something else.
+ *
+ * public FrequencyMap letterFrequency(String input)
+ * letter frequency, write a program that collects the frequency of each letter within the input.
+*/
+
 public class WordForWord {
+
+    // You'll need to setup a series of instance variables for the phases of processing
+    // you need to do on the text in the file(s).
+
+
     public static void main(String[] args) {
-        System.out.println("WordForWord");
+
+        WordForWord wfw = new WordForWord();
+
+        wfw.loadFile("testdata1.txt");
+
+        wfw.processAll();
+
+        System.out.println(wfw.toString());
     }
+
+    public String toString() {
+       return ""; // create a brief report on what you found out about the file contents.
+    }
+
+    private void processAll() {
+    }
+
+    private void loadFile(String file) {
+    }
+
 }
